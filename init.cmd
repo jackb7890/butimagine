7b4774779fa463d40b672fbc3a24447065219f39
jackb7890 <43074578+jackb7890@users.noreply.github.com>
2025-01-06 22:55:24 -0600

    new script just dropped (#8)
    Co-authored-by: iamjackb <johnbuchananece@gmail.com>

--- init.cmd
+++ init.cmd
@@ -1,6 +1,6 @@
 @echo OFF
 
-call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat"
-
-::CMake is bitch and wants file paths to be with forward slashes so here
-set VCPKG_ROOT_F=%VCPKG_ROOT:\=/%
+call powershell -executionpolicy remotesigned -file init.ps1 >> hehehe
+for /f "delims=" %%x in (hehehe) do set nowayitwasthathard=%%x
+call %nowayitwasthathard%
+del hehehe
