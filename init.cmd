92c707f06428a3c86ebd97d59723c6543008aff9
jackb7890 <43074578+jackb7890@users.noreply.github.com>
2025-01-07 09:55:26 -0600

    Added folder for scripts (#10)
    * lzbuild for lz people
    
    * make a scripts dir and put it on the path
    
    * fix
    
    ---------
    
    Co-authored-by: iamjackb <johnbuchananece@gmail.com>

--- init.cmd
+++ init.cmd
@@ -4,3 +4,5 @@ call powershell -executionpolicy remotesigned -file init.ps1 >> hehehe
 for /f "delims=" %%x in (hehehe) do set nowayitwasthathard=%%x
 call %nowayitwasthathard%
 del hehehe
+
+set PATH=%PATH%;%~dp0\scripts
