c476090d0908e844beccc038793fbb996b68a061
jackb7890 <jackthefirst@utexas.edu>
2025-01-07 18:01:06 -0600

    added walls? also switched to using references of map for player and wall

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <cstdio>
+#include <array>
 #include <windows.h>
 
 #define SDL_MAIN_HANDLED 1
@@ -16,11 +17,6 @@ void cleanup() {
 
 int main(int argc, char* argv[]) {
     init();
-    Map map;
-    map.SetStartMap();
-
-    // not sure how I feel about the map updating through the player class but fuck it right
-    Player player1(MAP_WIDTH/2, MAP_HEIGHT/2, &map);
 
     SDL_Window* win = SDL_CreateWindow( "my window", 100, 100, MAP_WIDTH, MAP_HEIGHT, SDL_WINDOW_SHOWN );
     if ( !win ) {
@@ -28,21 +24,52 @@ int main(int argc, char* argv[]) {
     }
 
     Display display(win);
-    printf("A\n");
+
+    Map map;
+    map.SetStartMap();
+
+    // not sure how I feel about the map updating through the player class but fuck it right
+    Player player1(MAP_WIDTH/2, MAP_HEIGHT/2, map);
 
     display.Update(map); // first draw of the map the screen (should include player initial pos)
-    printf("A\n");
     Sleep(1500);
-    printf("A\n");
-    printf("Z\n");
 
-    player1.MoveHoriz(50);
+    // short walls are 25 long
+    // long walls on bot/top are 50 long
+    // long back wall is 75 long
+
+    Wall lowerFront = Wall(205, 255, 25, true, map);
+    Wall bottom = Wall(155, 280, 50, false, map);
+    Wall back = Wall(155, 205, 75, true, map);
+    Wall top = Wall(155, 205, 50, false, map);
+    Wall upperFront = Wall(205, 205, 25, true, map);
+
+    display.Update(lowerFront);
+    Sleep(500);
+    display.Update(bottom);
+    Sleep(500);
+    display.Update(back);
+    Sleep(500);
+    display.Update(top);
+    Sleep(500);
+    display.Update(upperFront);
+    Sleep(500);
+
+    player1.MoveHoriz(-140);
     display.Update(player1);
     Sleep(1500);
 
-    player1.MoveVert(40);
+    player1.MoveVert(-20);
     display.Update(player1);
     Sleep(1500);
+
+    int shake = 5;
+    for (int i = 0; i < 75; i++) {
+        player1.MoveVert(shake);
+        display.Update(player1);
+        Sleep(20);
+        shake *= -1;
+    }
 
     cleanup();
     return 0;
