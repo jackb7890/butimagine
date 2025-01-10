b1e57c6dc1d959ab4b54c7d5d7370a7829f0cf6e
iamjackb <johnbuchananece@gmail.com>
2025-01-10 17:18:45 -0600

    fix bug where we werent wrapping indices for collision checker

---
+++ util.cpp
@@ -0,0 +1,12 @@
+int Wrap(int oldIndex, int change, int bound) {
+    int newIndex = oldIndex + change;
+    if (newIndex >= bound) {
+        return newIndex % bound;
+    }
+    else if (newIndex < 0) {
+        return bound + (newIndex % bound);
+    }
+    else {
+        return oldIndex + change;
+    }
+}
