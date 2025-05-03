8f3bee40a790193ab202f83a8fd0b013e54220bd
iamjackb <johnbuchananece@gmail.com>
2025-05-02 23:34:38 -0500

    fix up socket code, wrap data pointer and make data class

--- sockets/server.cpp
+++ sockets/server.cpp
@@ -1,8 +1,9 @@
-#include "SDL2/SDL_net.h"
+#include "SDL_net.h"
 #include <cstdio>
 #include <string.h>
 // tutorial maybe https://stephenmeier.net/2015/12/23/sdl-2-0-tutorial-04-networking/
-struct Packet {
+struct FlagsType {
+    bool isValid;
     union {
         unsigned short bits : 16;
         struct _flags {
@@ -17,6 +18,10 @@ struct Packet {
             unsigned short unused : 8;
         } flags;
     };
+
+    inline static constexpr size_t GetFlagsSize() {
+        return sizeof(flags);
+    }
     
     inline bool IsQuitCode() {
         return bits == 0; 
@@ -28,6 +33,59 @@ struct Packet {
 
     inline int WKeyRelease() {
         return flags.wKeyRelease;
+    }
+
+    inline void Init(uint16_t flagdata) {
+        bits = flagdata;
+        isValid = true;
+    }
+    
+    FlagsType() {
+        bits = 0xFEFE;
+        isValid = false;
+    }
+};
+
+struct Data {
+
+    bool isValid;
+    uint8_t* data;
+    FlagsType dataFlags;
+    size_t size; // size of data, not including the flag data
+    Data() : data(nullptr), isValid(false), size(0) {}
+
+    Data(uint8_t* rawTempData, int _size) {
+        // first 16-bits of one TCP message are going to stand for some status flags
+        dataFlags.Init(*reinterpret_cast<uint16_t*>(rawTempData));
+        int actualDataIndx = FlagsType::GetFlagsSize();
+
+        size = _size - actualDataIndx;
+
+        // assert this
+        if (data != nullptr) {
+            // assert
+        }
+        data = (uint8_t*) malloc((size)*sizeof(uint8_t));
+        memcpy(data, rawTempData + actualDataIndx, size);
+    }
+
+    bool GetRawData(uint8_t* out) {
+        if (!isValid) {
+            return false;
+        }
+        if (!out) {
+            return false;
+        }
+
+        *reinterpret_cast<uint16_t*>(out) = dataFlags.bits;
+        memcpy(out + FlagsType::GetFlagsSize(), data, size);
+        return true;
+    }
+
+    ~Data() {
+        if (data) {
+            free(data);
+        }
     }
 };
 
@@ -42,11 +100,10 @@ struct NetworkHelper {
     int next_ind;
     TCPsocket serverSoc;
     SDLNet_SocketSet socket_set;
-    TCPsocket clientSoc1;
-    bool soc1Setup;
+    TCPsocket clientSockets[MAX_SOCKETS];
     IPaddress ip;
 
-    NetworkHelper() : next_ind(0), soc1Setup(false) {
+    NetworkHelper() : next_ind(0) {
         init();
 
         if(SDLNet_ResolveHost(&ip, NULL, SERVER_PORT) == -1) {
@@ -77,14 +134,11 @@ struct NetworkHelper {
             fprintf(stderr, "ER: SDLNet_TCP_DelSocket: %sn", SDLNet_GetError());
             exit(-1);
         } SDLNet_TCP_Close(serverSoc);
-
-        CloseSocket(clientSoc1);
         
-        // int i;
-        // for(i=0; i<MAX_SOCKETS; ++i) {
-        //     if(sockets[i] == NULL) continue;
-        //     CloseSocket(i);
-        // }
+        for (int i=0; i<MAX_SOCKETS; ++i) {
+            if(clientSockets[i] == NULL) continue;
+            CloseSocket(clientSockets[i]);
+        }
         
         SDLNet_FreeSocketSet(socket_set);
         SDLNet_Quit();
@@ -92,68 +146,61 @@ struct NetworkHelper {
     }
 
     bool TryAddClient() {
-        if (soc1Setup) {
+        if (next_ind >= MAX_SOCKETS) {
+            // could choose to override sockets here, probably should
             return false;
         }
 
-        clientSoc1 = SDLNet_TCP_Accept(serverSoc);
+        clientSockets[next_ind] = SDLNet_TCP_Accept(serverSoc);
 
-        if (clientSoc1 == nullptr) {
+        if (clientSockets[next_ind] == nullptr) {
             return false;
         }
 
-        if (SDLNet_TCP_AddSocket(socket_set, clientSoc1) == -1) {
+        if (SDLNet_TCP_AddSocket(socket_set, clientSockets[next_ind]) == -1) {
             fprintf(stderr, "ER: SDLNet_TCP_AddSocket: %sn", SDLNet_GetError());
             exit(-1);
         }
     
         printf("DB: new connection (next_ind = %d)n", next_ind);
-
-        soc1Setup = true;
+        next_ind++;
         return true;
     }
 
-    void SendData(TCPsocket socket, uint8_t* data, uint16_t length, uint16_t flag) {
+    int SendData(int socketIndx, Data data) {
         uint8_t temp_data[MAX_PACKETS];
-    
-        int offset = 0;
-        memcpy(temp_data+offset, &flag, sizeof(uint16_t));
-        offset += sizeof(uint16_t);
-        memcpy(temp_data+offset, data, length);
-        offset += length;
-    
-        int num_sent = SDLNet_TCP_Send(sockets[index], temp_data, offset);
-        if(num_sent < offset) {
-            fprintf(stderr, "ER: SDLNet_TCP_Send: %sn", SDLNet_GetError());
-            CloseSocket(index);
+        if (!data.GetRawData(&temp_data[0])) {
+            printf("Error in GetRawData\n");
+            return -1;
         }
+
+        int num_sent = SDLNet_TCP_Send(clientSockets[socketIndx], temp_data, data.size);
+        if(num_sent < data.size) {
+            printf("ER: SDLNet_TCP_Send: %sn", SDLNet_GetError());
+            CloseSocket(clientSockets[socketIndx]);
+            return -1;
+        }
+
+        return num_sent; 
     }
 
-    uint8_t* RecvData(int index, uint16_t* length, uint16_t* flag) {
-        uint8_t temp_data[MAX_PACKET];
-        int num_recv = SDLNet_TCP_Recv(sockets[index], temp_data, MAX_PACKET);
+    Data RecvData(int index) {
+        uint8_t temp_data[MAX_PACKETS];
+        int num_recv = SDLNet_TCP_Recv(clientSockets[index], temp_data, MAX_PACKETS);
     
         if(num_recv <= 0) {
-            CloseSocket(index);
+            printf("Tried Receiving data on clientSocket: %d, but there TCP_Recv read no data\n Error: ");
+            CloseSocket(clientSockets[index]);
             const char* err = SDLNet_GetError();
             if(strlen(err) == 0) {
-                printf("DB: client disconnectedn");
+                printf("no error to emit\n");
             } else {
-                fprintf(stderr, "ER: SDLNet_TCP_Recv: %sn", err);
+                printf("SDLNet_TCP_Recv: %sn\n", err);
             }
     
-            return NULL;
+            return Data(); // empty constructor returns a non-valid Data object
         } else {
-            int offset = 0;
-            *flag = *(uint16_t*) &temp_data[offset];
-            offset += sizeof(uint16_t);
-    
-            *length = (num_recv-offset);
-    
-            uint8_t* data = (uint8_t*) malloc((num_recv-offset)*sizeof(uint8_t));
-            memcpy(data, &temp_data[offset], (num_recv-offset));
-    
-            return data;
+            return Data(&temp_data[0], num_recv);
         }
     }
 
@@ -202,34 +249,27 @@ int main() {
             ntwk.TryAddClient();
         }
 
-        if (SDLNet_SocketReady(ntwk.clientSoc1)) {
-            uint8_t* data;
-            uint16_t flag;
-            uint16_t length;
-                        
-            data = ntwk.RecvData(0, &length, &flag);
-            if(data == NULL) {
+        for (int socketIndx = 0; socketIndx < ntwk.MAX_SOCKETS; socketIndx++) {
+            if (!SDLNet_SocketReady(ntwk.clientSockets[socketIndx])) {
+                continue;
+            }
+
+            Data data = ntwk.RecvData(socketIndx);
+            if(!data.isValid) {
                 continue;
             }
         
-            switch(flag) {
+            switch(data.dataFlags.bits) {
                 case FLAG_WOOD_UPDATE: {
-                    uint16_t offset = 0;
-                    uint8_t send_data[MAX_PACKET];
-        
-                    memcpy(send_data+offset, &clients[ind].amt_wood, sizeof(uint8_t));
-                    offset += sizeof(uint8_t);
-        
-                    SendData(ind, send_data, offset, FLAG_WOOD_UPDATE);
+                    Data dataToSend; // TODO: fill this in with real data
+                    ntwk.SendData(socketIndx, dataToSend);
                 } break;
         
                 case FLAG_QUIT: {
-                    running = 0;
-                    printf("DB: shutdown by client id: %dn", ind);
+                    runLoop = false;
+                    printf("DB: shutdown by client id: %dn", socketIndx);
                 } break;
-            }
-        
-            free(data);
+            }        
         }
                 
 
